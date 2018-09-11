de9afb9d52c4adafb5b848137e50785b92b6e719
Farbod <farhour@gmail.com>
2018-09-11 13:04:30 +0300

    Mining the given repository and iterating through all the commits and finding all the commits that have at least one modification in a file with java extension.

---
+++ run.py
@@ -0,0 +1,19 @@
+import sys
+from pydriller import RepositoryMining
+import csv
+
+countOfArgs = len(sys.argv)
+pathToRepo = None
+if countOfArgs == 2:
+    pathToRepo = sys.argv[1]
+else:
+    pathToRepo = '../repository/'
+changes = []
+for commit in RepositoryMining(pathToRepo, only_modifications_with_file_types=['.java']).traverse_commits():
+        for modification in commit.modifications:
+            extOfFile = modification.filename[modification.filename.find('.') + 1:]
+            if extOfFile == 'java' and modification.change_type.name == 'MODIFY':
+                changes.append([commit.parents[0], modification.old_path, commit.hash, modification.new_path])
+with open('output/changes.csv', 'w') as myFile:
+    wr = csv.writer(myFile)
+    wr.writerows(changes)
