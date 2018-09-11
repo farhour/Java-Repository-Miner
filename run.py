9afe0f8cc7524cc97fd700a070f84eb4b0c4f21b
Farbod <farhour@gmail.com>
2018-09-11 16:11:54 +0300

    - Writing pathOfRepo to a csv file fot the next step. - Bug Fixing: A developer may change the java file and then rename or move it to another location. This type of modification should also be considered.

--- run.py
+++ run.py
@@ -9,12 +9,15 @@ if countOfArgs == 2:
     pathToRepo = sys.argv[1]
 else:
     pathToRepo = '../repository/'
+with open('output/pathToRepo.csv', 'w') as myFile:
+    myFile.write(pathToRepo)
 changes = []
 for commit in RepositoryMining(pathToRepo, only_modifications_with_file_types=['.java']).traverse_commits():
         for modification in commit.modifications:
             if modification.change_type is not None:
                 extOfFile = modification.filename[modification.filename.find('.') + 1:]
-                if extOfFile == 'java' and modification.change_type.name == 'MODIFY':
+                if extOfFile == 'java' and (modification.change_type.name == 'MODIFY') or \
+                        (modification.change_type.name == 'RENAME'):
                     changes.append([commit.parents[0], modification.old_path, commit.hash, modification.new_path])
 with open('output/changes.csv', 'w') as myFile:
     wr = csv.writer(myFile)
